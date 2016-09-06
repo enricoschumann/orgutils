14f4f6d5733b6b18afd9a963b45fecc4006c73b9
Enrico Schumann <es@enricoschumann.net>
2016-09-06 13:47:54 +0200

    Fix automatic running of unit tests

--- inst/unitTests/runTests.R
+++ inst/unitTests/runTests.R
@@ -2,15 +2,15 @@ if (require("RUnit", quietly = TRUE)) {
     localTesting <- TRUE
     pkg <- "org"
     require(pkg)
-    if (localTesting)
-        path <- paste0("~/Packages/", pkg, "/inst/unitTests" else
-    path <- system.file("unitTests", package = "org")
+    path <- if (localTesting)
+        paste0("~/Packages/", pkg, "/inst/unitTests") else
+        system.file("unitTests", package = "org")
 
     myTestSuite <- defineTestSuite(pkg, dirs = path,
-                                   testFileRegexp = "unitTests.+")
+                                   testFileRegexp = "ut_.+")
     stopifnot(isValidTestSuite(myTestSuite))
     testResult <- runTestSuite(myTestSuite, verbose = 0L)
     printTextProtocol(testResult, showDetails = TRUE,
-                      fileName = paste(file.path(path, "report"),
+                      fileName = paste(file.path(path, "test_results"),
                       ".txt", sep = ""))
 }
